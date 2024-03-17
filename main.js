b666170322db54d2adb9d78f77f7700272b12675
vishnu581 <vishnuvenugopal581@gmail.com>
2024-03-17 11:39:59 +0530

    1st commit, base 1 for button press

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 function show(){
     console.log("button clicked")}
+
+
+
+function game(){
+    let ans = prompt("Enter the Answers 5+5")
+    if(ans==10){
+        setTimeout(function () {
+            alert("Level 1 passed")
+        },2000)
+      }
+      else{
+        setTimeout(function () {
+            alert("Try again")
+        },2000)
+      }
+      
+}
